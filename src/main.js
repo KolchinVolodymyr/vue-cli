690b6beae5f4c02e4a0144ac7ffe17d17059733d
Volodymyr Kolchin <KolchinVolo@gmail.com>
2022-06-02 23:23:34 +0300

    commit

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import ActiveElement from './components/ActiveElement.vue';
-import KnowledgeBase from './components/KnowledgeBase.vue';
-import KnowledgeElement from './components/KnowledgeElement.vue';
-import KnowledgeGrid from './components/KnowledgeGrid.vue';
+import BaseCard from './components/UI/BaseCard.vue';
+import BaseButton from './components/UI/BaseButton.vue';
+import BaseDialog from './components/UI/BaseDialog.vue';
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.component('active-element', ActiveElement);
-app.component('knowledge-base', KnowledgeBase);
-app.component('knowledge-element', KnowledgeElement);
-app.component('knowledge-grid', KnowledgeGrid);
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-dialog', BaseDialog);
 
 app.mount('#app');
