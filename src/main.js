2e9d910a66815942db496a90cd8e2f90bac1508c
Volodymyr Kolchin <KolchinVolo@gmail.com>
2022-06-08 16:54:14 +0300

    commit

--- src/main.js
+++ src/main.js
@@ -1,23 +1,33 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
-import router from './router.js';
-import store from './store/index.js';
 import App from './App.vue';
-import BaseCard from './components/ui/BaseCard.vue';
-import BaseButton from './components/ui/BaseButton.vue';
-import BaseBadge from './components/ui/BaseBadge.vue';
-import BaseSpinner from './components/ui/BaseSpinner.vue';
-import BaseDialog from './components/ui/BaseDialog.vue';
 
-const app = createApp(App)
+const store = createStore({
+  state() {
+    return {
+      counter: 0
+    }
+  },
+  mutations: {
+    increment(state) {
+      state.counter++;
+    }
+  },
+  actions: {
+    increment(context) {
+      context.commit('increment');
+    }
+  },
+  getters: {
+    counter(state) {
+      return state.counter
+    }
+  }
+});
 
-app.use(router);
+const app = createApp(App);
+
 app.use(store);
-
-app.component('base-card', BaseCard);
-app.component('base-button', BaseButton);
-app.component('base-badge', BaseBadge);
-app.component('base-spinner', BaseSpinner);
-app.component('base-dialog', BaseDialog);
 
 app.mount('#app');
